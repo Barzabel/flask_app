955402b8a89673585f25762393bef79102c0d6b8
Barzabek <kochenko1995@mail.ru>
2023-10-29 16:31:45 +0300

    add two routses

--- flask_app/example.py
+++ flask_app/example.py
@@ -3,6 +3,17 @@ from flask import Flask
 # Это callable WSGI-приложение
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return "<H1>Hi, ***</h1>"
+def index():
+    return 'hi'
+
+
+@app.get('/users')
+def users_get():
+    return 'GET /users'
+
+
+@app.post('/users')
+def users_post():
+    return 'POST /users'
